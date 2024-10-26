8df5d3498c0a3f5e72fdc71d298e36b9840b0757
ryanchwang <ryanchwang@gmail.com>
2024-10-26 19:06:54 -0400

    table initial

--- Model/Table.java
+++ Model/Table.java
@@ -4,4 +4,19 @@ import org.w3c.dom.Node;
 
 public class Table {
 
+    private int round;
+    int[] hand = new int[5];
+    static Seat small = null;
+
+    public Table(){
+        this.round = round;
+        this.hand = hand;
+        this.small = small;
+    }
+    static void setOrder(Seat small) {}
+
+
+
+
 }
+
