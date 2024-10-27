fbadd489402f61380b95d0a2169ba39a3d953d72
Gen Shimizu <gen12shimizu@gmail.com>
2024-10-26 20:42:43 -0400

    created deck class

--- Model/Deck.java
+++ Model/Deck.java
@@ -1,13 +1,56 @@
 package Model;
+<<<<<<< Updated upstream
 
 public class Deck{
     private int numCards;
     private Card[] deck;
+=======
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+>>>>>>> Stashed changes
 
+public class Deck {
+    private HashSet<Card> deck;
 
-public Deck (int numCards){
-    this.numCards = numCards;
-    this.deck = new Card[numCards];
+    public Deck(){
+        deck = new HashSet<>();
+
+        for (int i = 0; i < 52; i++){
+            Card card = new Card(i/13, i%13);
+            deck.add(card);
+        }
+    }
+
+    public Card draw(){
+        if (deck.isEmpty()){
+            return null;
+        }
+
+        Card[] deckArray = deck.toArray(new Card[0]);
+        int pick = (int)(Math.random()*deck.size());
+        Card card = deckArray[pick];
+
+        deck.remove(card);
+
+        return card;
+    }
+
+    public HashSet<Card> newDeck(){
+        return deck;
+    }
+
+    public void reset(){
+        deck.clear();
+
+        for (int i = 0; i < deck.size(); i++){
+            Card card = new Card(i/13, i%13);
+            deck.add(card);
+        }
+
+    }
+
 }
-}
+   
+
 
